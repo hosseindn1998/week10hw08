93600afa78353033dc69a44a7dbf7b7c01d8ba75
hosseindn1998 <hosseindn1998@gmail.com>
2024-02-03 22:44:29 +0330

    Base> repository>baseRepository impel save method

--- src/main/java/base/repository/BaseRepositoryImpel.java
+++ src/main/java/base/repository/BaseRepositoryImpel.java
@@ -4,6 +4,9 @@ import base.model.BaseEntity;
 
 import java.io.Serializable;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class BaseRepositoryImpel<ID extends Serializable,TYPE extends BaseEntity<ID>>implements BaseRepository<ID,TYPE> {
 private final Connection connection;
@@ -15,5 +18,30 @@ private final Connection connection;
     public Connection getConnection() {
         return connection;
     }
+    @Override
+    public void save(TYPE entity) throws SQLException {
+
+        String sql = " INSERT INTO " + getTableName() + "  " + getColumnsName() +
+                " VALUES " + getCountOfQestionMarkParams();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            fillParamForStatement(preparedStatement, entity, false);
+            preparedStatement.executeUpdate();
+
+        }
+    }
+
+
+    public abstract String getTableName();
+    public abstract String getColumnsName();
+
+    public abstract String getCountOfQestionMarkParams();
+
+    public abstract void fillParamForStatement(PreparedStatement preparedStatement, TYPE entity, boolean isCountOnly)
+            throws SQLException;
+
+    public abstract TYPE mapResultSetToEntity(ResultSet resultSet) throws SQLException;
+
+    public abstract String getUpdateQueryParams();
 
 }
