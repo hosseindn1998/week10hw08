8888ec08c8e34bbec5ad3a95bd2ddc2f189c2c97
hosseindn1998 <hosseindn1998@gmail.com>
2024-02-04 09:11:29 +0330

    Model>admin fields

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,19 @@
+package model;
+
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level= AccessLevel.PRIVATE)
+public class Admin extends BaseEntity {
+    int id;
+    String fullName;
+    String username;
+    String password;
+
+
+}
